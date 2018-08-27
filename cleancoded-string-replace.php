c49c7f9af4be5c66ef1439058de8567f5b82bf16
James <james@cleancoded.com>
2018-08-26 22:26:00 -0500

    Update cleancoded-string-replace.php

--- cleancoded-string-replace.php
+++ cleancoded-string-replace.php
@@ -1,20 +1,20 @@
 <?php
 
 /*
- * Plugin Name: CLEANCODED String Replace
+ * Plugin Name: String Replace
  * Plugin URI: https://cleancoded.com/
- * Description: CLEANCODED String Replace replaces any defined string with another string within any WordPress website.
+ * Description: String Replace replaces any defined string with another string within any WordPress website.
  * Version: 1.0
  * Author: CLEANCODED
  * Author URI: https://cleancoded.com/
  * License: GPL
  * 
- *  CLEANCODED String Replace is free software: you can redistribute it and/or modify
+ *  String Replace is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 2 of the License, or
  *  any later version.
  * 
- *  CLEANCODED String Replace is distributed in the hope that it will be useful,
+ *  String Replace is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
